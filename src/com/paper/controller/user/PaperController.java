00ec153509813e2619bcac90d9fad45e6eece113
moon <hoop_ster@naver.com>
2017-08-28 19:31:08 +0900

    paperReg fix

--- src/com/paper/controller/user/PaperController.java
+++ src/com/paper/controller/user/PaperController.java
@@ -3,6 +3,7 @@ package com.paper.controller.user;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
@@ -32,5 +33,33 @@ public class PaperController {
 	//	nNo = null;
 		log.info(this.getClass().getName() + " userPaperReg End!!");
 		return "paperReg";
+	}
+	
+	@RequestMapping(value="paperRegProc")
+	public String paperRegProc(HttpServletRequest req, HttpServletResponse res, Model model) throws Exception{
+		log.info(this.getClass().getName()+ " paperRegProc Start!!");
+		
+		String paperKor = req.getParameter("korName");
+		String paperEng = req.getParameter("engName");
+		String writerNames[] = req.getParameterValues("name");
+		String writerEmails[] = req.getParameterValues("email");
+		String writerBelongs[] = req.getParameterValues("belong");
+		
+		log.info(paperKor);
+		log.info(paperEng);
+		
+		log.info("---------------------------------");
+		for(int i=0; i<writerNames.length;i++){
+			String writerName = writerNames[i];
+			String writerEmail = writerEmails[i];
+			String writerBelong = writerBelongs[i];
+			log.info(writerName);
+			log.info(writerEmail);
+			log.info(writerBelong);
+		}
+		log.info("---------------------------------");
+		
+		log.info(this.getClass().getName()+ " paperRegProc End!!");
+		return "redirect:paperReg.do";
 	}
 }
